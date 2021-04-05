c4db2cb471d9471b1d6959ff2db3453ef0390381
sukhada111 <sukhada9987@gmail.com>
2021-04-05 21:19:47 +0530

    created apps and folders

--- Moviezilla_project/Moviezilla/settings.py
+++ Moviezilla_project/Moviezilla/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'userprofile',
+    'recommend',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL='/media/'
+
+MEDIA_ROOT= BASE_DIR / 'media'
+

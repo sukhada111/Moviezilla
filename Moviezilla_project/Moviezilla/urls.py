71aa77b496fb38c09d63e8dff9c508f6471d961d
sukhada111 <sukhada9987@gmail.com>
2021-04-08 00:34:14 +0530

    implemented dashboard and changes to index

--- Moviezilla_project/Moviezilla/urls.py
+++ Moviezilla_project/Moviezilla/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from home import views
 from register import views as v
+from recommend import views as vr
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +14,8 @@ urlpatterns = [
     # path('', include("django.contrib.auth.urls")),
     path('logout/', v.logoutuser,name='logoutuser'),
     path('login/', v.loginuser,name='loginuser'),
+    path('dashboard/', vr.dashb,name='dashb'),
+
     
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 

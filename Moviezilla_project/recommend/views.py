4a25029ac8dd4b573f5c062a77e0b065db421467
sukhada111 <sukhada9987@gmail.com>
2021-04-17 11:44:04 +0530

    implemented actor name recommendation

--- Moviezilla_project/recommend/views.py
+++ Moviezilla_project/recommend/views.py
@@ -104,29 +104,67 @@ from sklearn.metrics.pairwise import cosine_similarity
 similarity_matrix = cosine_similarity(X)
 
 
-
 mov=np.array(final['lower_title'])
+
+#case for actor name entered to get recommendations
+def convert_cast(name):
+    l1=[]
+    for i in name:
+        l1.append(i.lower())
+    return l1
+
+final['lower_cast']=final['cast'].apply(convert_cast)
+
+#list of lists of cast for each movie
+listt_cast=final['lower_cast'].tolist()
+
 
 #loading the saved pickle Models
 # cv=pickle.load(open('C:/Users/sukha/Documents/Moviezilla/Moviezilla_project/recommend/static/recommend/cv.sav','rb'))
 # similarity_matrix=pickle.load(open('C:/Users/sukha/Documents/Moviezilla/Moviezilla_project/recommend/static/recommend/similarity_matrix.sav','rb'))
 
-#function to find similar movies
+#function to find similar movies, cases: movie name is entered or actor name is entered by user
 def recommend(movie_name):
     recs=[]
+    flag=0
     movie_name=movie_name.lower()
+    
+    #checking if movie name is present
     if movie_name in mov:
+        flag=1
+        
         movie_index=final[final['lower_title']== movie_name].index[0]
-
+        
+    #case when actor name is entered
+    else:
+        actor_name=movie_name.lower()
+        # checking if its present
+        for i in range(len(listt_cast)):
+            if actor_name in listt_cast[i]:
+                
+                flag=2
+                
+                title= final.iloc[i]['lower_title']
+                break
+        if flag==2:
+            movie_index=final[final['lower_title']== title].index[0]
+        
+        
+    if flag==1 or flag==2:
+        
+        #finding movies similar to given movie
         L=sorted(list(enumerate(similarity_matrix[movie_index])),reverse=True,key=lambda x:x[1])
         
+        #recommending top 5 most similar movies
         for i in L[1:6]:
             print(final.iloc[i[0]]['original_title'])
             recs.append(final.iloc[i[0]]['original_title'])
         return recs
+    
+    #if movie/actor name not present in dataset
     else:
         return recs
-
+        
 
 # Create your views here.
 
